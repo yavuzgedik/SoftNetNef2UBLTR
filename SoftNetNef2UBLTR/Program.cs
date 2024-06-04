6a9f337ec914d6524b1009bb786606da9a7c37c7
yavuzgedik <yavuzgedik@outlook.com>
2024-06-04 20:29:13 +0300

    Add project files.

---
+++ SoftNetNef2UBLTR/Program.cs
@@ -0,0 +1,260 @@
+﻿using SoftNetNef2UBLTR.SoftNetWS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using static System.Net.WebRequestMethods;
+
+namespace SoftNetNef2UBLTR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanılan UBLTR Kütüphane : https://github.com/hkutluay/UblTr
+
+            var nefEarsiv = SoftNetOP.GetEarsiv();
+
+            #region Vergiler
+
+            var taxTotal = new UblTr.Common.TaxTotalType[]
+            {
+                new UblTr.Common.TaxTotalType()
+                {
+                    TaxAmount = new UblTr.Common.TaxAmountType()
+                    {
+                        Value = Convert.ToDecimal(nefEarsiv.KDVTutari),
+                        currencyID = nefEarsiv.DovizTipi,
+                    },
+                    TaxSubtotal = new UblTr.Common.TaxSubtotalType[nefEarsiv.Vergiler.Length]
+                }
+            };
+
+            for (var i = 0; i < nefEarsiv.Vergiler.Length; i++)
+            {
+                taxTotal[0].TaxSubtotal[i] = new UblTr.Common.TaxSubtotalType()
+                {
+                    TaxableAmount = new UblTr.Common.TaxableAmountType()
+                    {
+                        Value = Convert.ToDecimal(nefEarsiv.Vergiler[i].Matrah),
+                        currencyID = nefEarsiv.DovizTipi,
+                    },
+                    TaxAmount = new UblTr.Common.TaxAmountType()
+                    {
+                        Value = Convert.ToDecimal(nefEarsiv.Vergiler[i].Tutar),
+                        currencyID = nefEarsiv.DovizTipi,
+                    },
+                    Percent = Convert.ToDecimal(nefEarsiv.Vergiler[i].Oran),
+                    TaxCategory = new UblTr.Common.TaxCategoryType()
+                    {
+                        TaxScheme = new UblTr.Common.TaxSchemeType()
+                        {
+                            // Türe Vergi Adı Getirilmeli
+                            // Item0015 : GERÇEK USULDE KATMA DEĞER VERGİSİ
+                            Name = nefEarsiv.Vergiler[i].Tur.ToString(),
+                            TaxTypeCode = nefEarsiv.Vergiler[i].Tur.ToString().Replace("Item", ""),
+                        }
+                    }
+                };
+            }
+
+            #endregion
+
+            #region Kalemler
+
+            var invoiceLines = new UblTr.Common.InvoiceLineType[nefEarsiv.FaturaKalemleri.Length];
+
+            for (var i = 0; i < nefEarsiv.FaturaKalemleri.Length; i++)
+            {
+                var kalem = nefEarsiv.FaturaKalemleri[i];
+                invoiceLines[i] = new UblTr.Common.InvoiceLineType()
+                {
+                    ID = new UblTr.Common.IDType() { Value = (i + 1).ToString() },
+                    InvoicedQuantity = new UblTr.Common.InvoicedQuantityType()
+                    {
+                        unitCode = kalem.Birim,
+                        Value = Convert.ToDecimal(kalem.Miktar),
+                    },
+                    LineExtensionAmount = new UblTr.Common.LineExtensionAmountType()
+                    {
+                        Value = Convert.ToDecimal(kalem.Vergiler[0].Matrah),
+                        currencyID = nefEarsiv.DovizTipi
+                    },
+                    OrderLineReference = new UblTr.Common.OrderLineReferenceType[]
+                    {
+                        new UblTr.Common.OrderLineReferenceType()
+                        {
+                            LineID = new UblTr.Common.LineIDType()
+                            {
+                                Value = (i + 1).ToString(),
+                            }
+                        }
+                    },
+                    TaxTotal = new UblTr.Common.TaxTotalType()
+                    {
+                        TaxAmount = new UblTr.Common.TaxAmountType()
+                        {
+                            Value = Convert.ToDecimal(kalem.Vergiler[0].Tutar),
+                            currencyID = nefEarsiv.DovizTipi,
+                        },
+                        TaxSubtotal = new UblTr.Common.TaxSubtotalType[]
+                        {
+                            new UblTr.Common.TaxSubtotalType()
+                            {
+                                TaxableAmount = new UblTr.Common.TaxableAmountType()
+                                {
+                                    Value = Convert.ToDecimal(kalem.Vergiler[0].Matrah),
+                                    currencyID = kalem.DovizTipi,
+                                },
+                                TaxAmount = new UblTr.Common.TaxAmountType()
+                                {
+                                    Value = Convert.ToDecimal(kalem.Vergiler[0].Tutar),
+                                    currencyID = nefEarsiv.DovizTipi,
+                                },
+                                Percent = Convert.ToDecimal(kalem.Vergiler[0].Oran),
+                                TaxCategory = new UblTr.Common.TaxCategoryType()
+                                {
+                                    TaxScheme = new UblTr.Common.TaxSchemeType()
+                                    {
+                                        // Türe Göre Vergi Adı Getirilmeli
+                                        // Item0015 : GERÇEK USULDE KATMA DEĞER VERGİSİ
+                                        Name = kalem.Vergiler[0].Tur.ToString(),
+                                        TaxTypeCode = kalem.Vergiler[0].Tur.ToString().Replace("Item", ""),
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+
+            #endregion
+
+            var invoice = new UblTr.MainDoc.InvoiceType()
+            {
+                UUID = nefEarsiv.GUID,
+                UBLVersionID = "2.1",
+                CustomizationID = "TR1.2",
+                ProfileID = nefEarsiv.Senaryo.ToString(),
+                ID = nefEarsiv.No,
+                CopyIndicator = false,
+                AccountingSupplierParty = new UblTr.Common.SupplierPartyType()
+                {
+                    Party = new UblTr.Common.PartyType()
+                    {
+                        PartyIdentification = new UblTr.Common.PartyIdentificationType[]
+                        {
+                            new UblTr.Common.PartyIdentificationType()
+                            {
+                                ID = nefEarsiv.Tedarikci.VergiNoTCKimlikNo,
+                            },
+                        },
+                        PartyName = new UblTr.Common.PartyNameType()
+                        {
+                            Name = nefEarsiv.Tedarikci.FirmaAdi
+                        },
+                        PostalAddress = new UblTr.Common.AddressType()
+                        {
+                            StreetName = nefEarsiv.Tedarikci.Sokak,
+                            CitySubdivisionName = nefEarsiv.Tedarikci.IlceSemt,
+                            CityName = nefEarsiv.Tedarikci.Il,
+                            PostalZone = nefEarsiv.Tedarikci.PostaKodu,
+                            Country = new UblTr.Common.CountryType()
+                            {
+                                Name = nefEarsiv.Tedarikci.Ulke
+                            }
+                        },
+                        PartyTaxScheme = new UblTr.Common.PartyTaxSchemeType()
+                        {
+                            TaxScheme = new UblTr.Common.TaxSchemeType()
+                            {
+                                Name = nefEarsiv.Tedarikci.VergiDairesi
+                            }
+                        },
+                        Person = new UblTr.Common.PersonType()
+                        {
+                            // UYARI : Geçici Çözüm
+                            FirstName = nefEarsiv.Tedarikci.FirmaAdi.Split(' ').First(), // İsim
+                            FamilyName = nefEarsiv.Tedarikci.FirmaAdi.Split(' ').Last(), // Soyisim
+                        }
+                    }
+                },
+                AccountingCustomerParty = new UblTr.Common.CustomerPartyType()
+                {
+                    Party = new UblTr.Common.PartyType()
+                    {
+                        PartyIdentification = new UblTr.Common.PartyIdentificationType[]
+                        {
+                            new UblTr.Common.PartyIdentificationType()
+                            {
+                                ID = nefEarsiv.Musteri.VergiNoTCKimlikNo,
+                            },
+                        },
+                        PartyName = new UblTr.Common.PartyNameType()
+                        {
+                            Name = nefEarsiv.Musteri.FirmaAdi
+                        },
+                        PostalAddress = new UblTr.Common.AddressType()
+                        {
+                            StreetName = nefEarsiv.Musteri.Sokak,
+                            CitySubdivisionName = nefEarsiv.Musteri.IlceSemt,
+                            CityName = nefEarsiv.Musteri.Il,
+                            PostalZone = nefEarsiv.Musteri.PostaKodu,
+                            Country = new UblTr.Common.CountryType()
+                            {
+                                Name = nefEarsiv.Musteri.Ulke
+                            }
+                        },
+                        Person = new UblTr.Common.PersonType()
+                        {
+                            // UYARI : Geçici Çözüm
+                            FirstName = nefEarsiv.Musteri.FirmaAdi.Split(' ')[0], // İsim
+                            FamilyName = nefEarsiv.Musteri
+                            .FirmaAdi.Substring(nefEarsiv.Musteri.FirmaAdi.IndexOf(' ') + 1) // Soyisim
+                        }
+                    }
+                },
+
+                TaxTotal = taxTotal,
+
+                LegalMonetaryTotal = new UblTr.Common.MonetaryTotalType()
+                {
+                    LineExtensionAmount = new UblTr.Common.LineExtensionAmountType()
+                    {
+                        Value = Convert.ToDecimal(nefEarsiv.KDVMatrahi),
+                        currencyID = nefEarsiv.DovizTipi
+                    },
+                    TaxExclusiveAmount = new UblTr.Common.TaxExclusiveAmountType
+                    {
+                        Value = Convert.ToDecimal(nefEarsiv.KDVMatrahi),
+                        currencyID = nefEarsiv.DovizTipi
+                    },
+                    TaxInclusiveAmount = new UblTr.Common.TaxInclusiveAmountType()
+                    {
+                        Value = Convert.ToDecimal(nefEarsiv.OdenecekToplamTutar),
+                        currencyID = nefEarsiv.DovizTipi
+                    },
+                    PayableAmount = new UblTr.Common.PayableAmountType()
+                    {
+                        Value = Convert.ToDecimal(nefEarsiv.OdenecekToplamTutar),
+                        currencyID = nefEarsiv.DovizTipi
+                    }
+                },
+
+                InvoiceLine = invoiceLines,
+
+            };
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(UblTr.MainDoc.InvoiceType));
+
+            using (TextWriter writer = new StreamWriter(@"./sample.xml"))
+            {
+                xmlSerializer.Serialize(writer, invoice, new UblTr.Serialization.UblTrNamespaces());
+            }
+        }
+    }
+}
---
+++ SoftNetNef2UBLTR/SoftNetOP.cs
@@ -0,0 +1,122 @@
+﻿using SoftNetNef2UBLTR.SoftNetWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftNetNef2UBLTR
+{
+    public static class SoftNetOP
+    {
+        public static NetleBelge GetEarsiv() 
+        {
+            var nef = new NetleEFatura();
+
+            var uniqueId = Guid.NewGuid();
+
+            nef.GUID = uniqueId.ToString();// ETTN
+
+            nef.Tedarikci = new Tedarikci()
+            {
+                FirmaAdi = "Tedarikci Firma LTD",
+                VergiNoTCKimlikNo = "10203040506",
+                Il = "İzmir",
+                IlceSemt = "Konak",
+                Ulke = "Türkiye",
+                PostaKodu = "35000",
+                VergiDairesi = "KONAK",
+                Sokak = "123 Sokak No 35 ",
+            };
+
+            nef.Musteri = new Musteri()
+            {
+                FirmaAdi = "Muhtelif Müşteri",
+                VergiNoTCKimlikNo = "11111111111",
+                Il = "İzmir",
+                IlceSemt = "Konak",
+                Ulke = "TÜRKİYE",
+                PostaKodu = "35000",
+                VergiDairesi = "KONAK",
+                Sokak = "Akdeniz Mah. Akdeniz Cad. No : 3/8",
+            };
+
+            nef.DuzenlenmeTarihi = DateTime.Now;
+            nef.No = "SNA2024000000001";
+
+            nef.DovizTipi = "TRY";
+            nef.Tip = NetleEFaturaType.SATIS;
+            nef.Senaryo = NetleEFaturaSenaryoType.TEMELFATURA;
+            nef.KaynakDokumanTuru = KaynakDokumanTuru.EARSIV;
+
+            nef.FaturaKalemleri = new FaturaKalemi[]
+            {
+                new FaturaKalemi()
+                {
+                    Miktar = 2,
+                    StokAdi = "Hizmet Bedeli",
+                    Birim = "NIU",
+                    BirimFiyat = 10,
+                    ToplamTutar = 20,
+                    DovizTipi = "TRY",
+                    Aciklama = "Yazılım Geliştirme Hizmeti",
+                    Vergiler = new Vergi[]
+                    {
+                        new Vergi()
+                        {
+                            Matrah = 20,
+                            Oran = 20,
+                            Tur = TaxCodeContentType.Item0015,
+                            Tutar = 4,
+                        }
+                    }
+                },
+                new FaturaKalemi()
+                {
+                    Miktar = 1,
+                    StokAdi = "Hizmet Bedeli 2",
+                    Birim = "NIU",
+                    BirimFiyat = 10,
+                    ToplamTutar = 10,
+                    DovizTipi = "TRY",
+                    Aciklama = "Yazılım Geliştirme Hizmeti",
+                    Vergiler = new Vergi[]
+                    {
+                        new Vergi()
+                        {
+                            Matrah = 10,
+                            Oran = 10,
+                            Tur = TaxCodeContentType.Item0015,
+                            Tutar = 1,
+                        }
+                    }
+                }
+            };
+
+            nef.Vergiler = new Vergi[]
+            {
+                new Vergi()
+                {
+                    Matrah = 20,
+                    Oran = 20,
+                    Tur = TaxCodeContentType.Item0015,
+                    Tutar = 2,
+                },
+                new Vergi()
+                {
+                    Matrah = 10,
+                    Oran = 10,
+                    Tur = TaxCodeContentType.Item0015,
+                    Tutar = 1,
+                }
+            };
+
+            nef.ToplamTutar = 23;
+            nef.KDVMatrahi = 20;
+            nef.KDVTutari = 3;
+            nef.OdenecekToplamTutar = 23;
+
+            return nef;
+        }
+    }
+}
